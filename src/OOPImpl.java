92248f5bc0868fb132684daca31055d624684061
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-02-19 14:17:44 +0530

    OOP's implementation

--- src/OOPImpl.java
+++ src/OOPImpl.java
@@ -1,5 +1,31 @@
+//class Pen{
+//    String color;
+//    String type;
+//    public void write(){
+//        System.out.println("writing");
+//    }
+//}
+
+// this keyword: this is a reference variable that refers to the current object,
+// or can be said “this” in Java is a keyword that refers to the current object instance
+
+class Student{
+    String name;
+    Integer age;
+
+    // Constructor
+    Student(String name, Integer age){
+        this.name = name;
+        this.age = age;
+    }
+    public void printInfo(){
+        System.out.println("Name: " + this.name + " \nage: " + this.age);
+    }
+}
+
 public class OOPImpl {
     public static void main(String[] args) {
-        System.out.println("OOP's");
+        Student student1 = new Student("Student 1", 20);
+        student1.printInfo();
     }
 }
